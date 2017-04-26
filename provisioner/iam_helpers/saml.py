6964a76a5f53e0d8316366762a700fe6c2b125a4
Rob Lugton <rob.lugton@Robs-MacBook-Pro.local>
2017-04-26 17:00:05 +1000

    Python3 formatted print statements

--- provisioner/iam_helpers/saml.py
+++ provisioner/iam_helpers/saml.py
@@ -17,10 +17,10 @@ def add_saml_provider(metadata_file_name, saml_provider_name):
         return response["SAMLProviderArn"]
     except ClientError as client_error:
         if client_error.response['Error']['Code'] == 'EntityAlreadyExists':
-            print "SAML provider already exists..."
+            print ("SAML provider already exists...")
             return lookup_saml_provider(saml_provider_name)
         else:
-            print "There was an error creating the SAML provider: " + str(client_error)
+            print ("There was an error creating the SAML provider: " + str(client_error))
 
 
 def lookup_saml_provider(saml_provider_name):
@@ -28,9 +28,9 @@ def lookup_saml_provider(saml_provider_name):
     try:
         saml_providers = __iam_client__.list_saml_providers()
     except ClientError as client_error:
-        print "Unable to retrieve list of SAML providers: " + str(client_error)
+        print ("Unable to retrieve list of SAML providers: " + str(client_error))
     for prov in saml_providers['SAMLProviderList']:
         if saml_provider_name in prov['Arn']:
             return prov['Arn']
-    print "Couldn't find ARN of SAML Provider " + saml_provider_name
+    print ("Couldn't find ARN of SAML Provider " + saml_provider_name)
     return None
