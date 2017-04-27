4f73ac0df760cb0e45b32b8027c4a0e2939dcacf
Rob Lugton <rob.lugton@Robs-MacBook-Pro.local>
2017-04-27 13:50:38 +1000

    Add some exception handling to the role creation function
    Added a custom exception for when a role exists.

--- provisioner/iam_helpers/exceptions.py
+++ provisioner/iam_helpers/exceptions.py
@@ -12,7 +12,17 @@ class SAMLProviderExistsError(Error):
     Exception raised when the SAML Provider already exists in the AWS Account
     """
     def __init__(self, provider):
-        message = "SAML Provider {} already exists in this account.".format(provider)
+        message = "SAML Provider '{}' already exists in this account.".format(provider)
         super().__init__(message)
         self.provider = provider
+        self.message = message
+
+class TrustRoleExistsError(Error):
+    """
+    Exception raised when the trust role already exists in the AWS Account
+    """
+    def __init__(self, trust_role):
+        message = "Trust role '{}' already exists in this account.".format(trust_role)
+        super().__init__(message)
+        self.trust_role = trust_role
         self.message = message
--- provisioner/iam_helpers/roles.py
+++ provisioner/iam_helpers/roles.py
@@ -3,19 +3,30 @@ Utility functions for AWS IAM role-based activities
 """
 import json
 import boto3
+from botocore.exceptions import ClientError
+from .exceptions import TrustRoleExistsError
 
 __iam_client__ = boto3.client('iam')
 
 def add_trust_role(federated_role_template_file, saml_provider_arn, role_name, role_description):
     "Adds a role with the federated identity provider set to `saml_provider_arn`"
-    role_definition_base = open(federated_role_template_file, 'rU').read()
-    role_definition_json = json.loads(role_definition_base)
-    role_definition_json["Statement"][0]["Principal"]["Federated"] = saml_provider_arn
-    role_definition = json.dumps(role_definition_json)
-    response = __iam_client__.create_role(
-        RoleName=role_name,
-        AssumeRolePolicyDocument=role_definition,
-        Description=role_description
-    )
-    return response["Role"]["Arn"]
-
+    try:
+        role_definition_json = json.load(open(federated_role_template_file))
+        role_definition_json["Statement"][0]["Principal"]["Federated"] = saml_provider_arn
+        role_definition = json.dumps(role_definition_json)
+        response = __iam_client__.create_role(
+            RoleName=role_name,
+            AssumeRolePolicyDocument=role_definition,
+            Description=role_description
+        )
+        return response["Role"]["Arn"]
+    except FileNotFoundError:
+        print("Unable to locate template file {}".format(federated_role_template_file))
+        raise
+    except ClientError as client_error:
+        if client_error.response['Error']['Code'] == 'EntityAlreadyExists':
+            print("SAML provider already exists...")
+            raise TrustRoleExistsError(role_name)
+        else:
+            print("Something went wrong creating the Trust Role: {}". format(client_error))
+            raise
