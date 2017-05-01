94e06b7893b587a4fd644eda6c74dc42c3d6ebbd
Rob Lugton <rob.lugton@Robs-MacBook-Pro.local>
2017-05-01 11:02:06 +1000

    Changed the template variable to template_path to make it clearer what it's for

--- provisioner/cfn_helpers/templates.py
+++ provisioner/cfn_helpers/templates.py
@@ -8,21 +8,21 @@ from provisioner.exceptions import InvalidTemplateError
 
 __client__ = boto3.client('cloudformation', region_name='ap-southeast-2')
 
-def validate_template(template):
+def validate_template(template_path):
     """
     Validate a template is correct.
     """
     try:
-        with open(template, 'r') as template_body:
+        with open(template_path, 'r') as template_body:
             response = __client__.validate_template(TemplateBody=template_body.read())
             return response
     except FileNotFoundError:
-        print("Unable to locate template file {}".format(template))
+        print("Unable to locate template file {}".format(template_path))
         raise
     except ClientError as client_error:
         if client_error.response['Error']['Code'] == 'ValidationError':
             print("SAML provider already exists...")
-            raise InvalidTemplateError(template, client_error.response['Error']['Message'])
+            raise InvalidTemplateError(template_path, client_error.response['Error']['Message'])
         else:
             print("There was an error validating the template: " + str(client_error))
             raise
