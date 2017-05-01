baf9dacd8f1ea9ea2cdbdb7a94d2d3e3175dcda8
Rob Lugton <rob.lugton@Robs-MacBook-Pro.local>
2017-05-01 11:14:05 +1000

    Add Error handling for create_stack
    Returns a StackExistsError if a stack already exists when trying to create it.

--- provisioner/cfn_helpers/stacks.py
+++ provisioner/cfn_helpers/stacks.py
@@ -3,23 +3,38 @@ Collection of functions for dealing with cloudformation stacks.
 """
 
 import boto3
+from botocore.exceptions import ClientError
+from provisioner.exceptions import StackExistsError
 
 __client__ = boto3.client("cloudformation", region_name="ap-southeast-2")
 
-def create_stack(stack_name, template, parameters):
+def create_stack(stack_name, template_path, parameters):
     """
     Create a cloudformation stack in the current account
     """
-    response = __client__.create_stack(
-        StackName=stack_name,
-        TemplateBody=template,
-        Parameters=parameters,
-        TimeoutInMinutes=5,
-        Capabilities=[
-            'CAPABILITY_NAMED_IAM'
-        ],
-        OnFailure='ROLLBACK'
-    )
-    print(response)
+    try:
+        with open(template_path, 'r') as template_body:
+            response = __client__.create_stack(
+                StackName=stack_name,
+                TemplateBody=template_body.read(),
+                Parameters=parameters,
+                TimeoutInMinutes=5,
+                Capabilities=[
+                    'CAPABILITY_NAMED_IAM'
+                ],
+                OnFailure='ROLLBACK'
+            )
+            print(response)
 
-    return response['StackId']
+            return response['StackId']
+    except FileNotFoundError:
+        print("Unable to locate template file {}".format(template_path))
+        raise
+    except ClientError as client_error:
+        print("Fooooo")
+        if client_error.response['Error']['Code'] == 'AlreadyExistsException':
+            print("SAML provider already exists...")
+            raise StackExistsError(stack_name)
+        else:
+            print("There was an error validating the template: " + str(client_error))
+            raise
