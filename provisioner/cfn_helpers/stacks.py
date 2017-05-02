13801916937be774421fd867c9575ef4db8cec50
Rob Lugton <rob.lugton@Robs-MacBook-Pro.local>
2017-05-02 10:59:09 +1000

    Now prints out a table of stack events if stack creation fails

--- provisioner/cfn_helpers/stacks.py
+++ provisioner/cfn_helpers/stacks.py
@@ -2,6 +2,7 @@
 Collection of functions for dealing with cloudformation stacks.
 """
 
+from beautifultable import BeautifulTable
 import boto3
 from botocore.exceptions import ClientError, WaiterError
 from provisioner.exceptions import StackExistsError
@@ -26,23 +27,59 @@ def create_stack(stack_name, template_path, parameters):
                 OnFailure='ROLLBACK'
             )
             print(response)
-
             print("Waiting for stack creation to complete...")
-            complete_waiter.wait(stack_name)
-
+            complete_waiter.wait(StackName=stack_name)
             return response['StackId']
     except FileNotFoundError:
         print("Unable to locate template file {}".format(template_path))
         raise
     except WaiterError as waiter_error:
         print("Something went wrong creating the stack! {}".format(waiter_error))
-        # need to add some extra debug stuff here.
+        print_stack_events(stack_name, 150)
         raise
     except ClientError as client_error:
-        print("Fooooo")
         if client_error.response['Error']['Code'] == 'AlreadyExistsException':
             print("SAML provider already exists...")
             raise StackExistsError(stack_name)
         else:
             print("There was an error validating the template: " + str(client_error))
             raise
+
+def get_stack_events(stack_name):
+    """
+    Return a simplified dict of stack events
+    """
+
+    try:
+        response = __client__.describe_stack_events(StackName=stack_name)
+        events = []
+        for event in response['StackEvents']:
+            timestamp = None
+            resource = None
+            status = None
+            reason = None
+            try:
+                timestamp = event['Timestamp'].astimezone()
+                resource = event['LogicalResourceId']
+                status = event['ResourceStatus']
+                reason = event['ResourceStatusReason']
+            except KeyError:
+                pass
+            events.append([timestamp, resource, status, reason])
+        return events
+    except ClientError:
+        print("Error describing stack {}".format(stack_name))
+        raise
+
+def print_stack_events(stack_name, table_width=80):
+    """
+    Print out stack events in a pretty table
+    """
+    stack_events = get_stack_events(stack_name)
+    table = BeautifulTable(max_width=table_width)
+    table.column_headers = ["Timestamp", "Resource", "Status", "Message"]
+    for event in stack_events:
+        table.append_row(event)
+    table.sort('Timestamp')
+    print("Stack Events:")
+    print(table)
