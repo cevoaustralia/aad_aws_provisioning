625500495fe3255520544a6af09399a3ca36d236
Rob Lugton <rob.lugton@Robs-MacBook-Pro.local>
2017-05-03 10:39:11 +1000

    Deprovision the stack and SAML provider

--- deprovisioner.py
+++ deprovisioner.py
@@ -2,6 +2,9 @@
 Tear down a provisioned stack and remove the SAML provider for cleanup.
 """
 
+from provisioner.cfn_helpers.stacks import delete_stack
+from provisioner.iam_helpers.saml import look_up_saml_provider, delete_saml_provider
+
 def main(args):
     """
     Tear it down!
@@ -10,9 +13,14 @@ def main(args):
 
 # firstly tear down the cloudformation stack
     print("Removing stack '{}'".format(args.stack_name))
+    delete_stack(args.stack_name)
 
 # then remove the SAML provider.
     print("Removing SAML provider '{}'".format(args.saml_provider_name))
+    # need the ARN of the saml provider, so lets look it up
+
+    saml_provider_arn = look_up_saml_provider(args.saml_provider_name)
+    delete_saml_provider(saml_provider_arn)
 
 if __name__ == "__main__":
     import argparse
@@ -21,11 +29,13 @@ if __name__ == "__main__":
                             type=str,
                             help='name of the cloudformation stack',
                             dest='stack_name',
+                            required=True,
                             default='federated-trust-role-and-policy')
     __parser__.add_argument('-p --saml_provider_name',
                             type=str,
                             help='name to assign to the identity provider',
                             dest="saml_provider_name",
+                            required=True,
                             default="new_saml_provider")
     __args__ = __parser__.parse_args()
     main(__args__)
